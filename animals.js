98476fab39b2507a641349254d9447bd1229d680
Floresda <97864447+Floresda@users.noreply.github.com>
2024-02-18 21:35:56 -0500

    animals.js

--- animals.js
+++ animals.js
@@ -1,18 +1,17 @@
 function showInfo() {
-    const input = document.getElementById("petNum").ariaValueMax;
+    const input = document.getElementById("petNum").value;
     const petNumber = parseInt(input);
-
-
-if (isNaN(petNumber), petNumber < 1, petNumber > petsData.length) {
-    alert("Please enter a valid number between 1 and " + petstData.length);
-    return;
-}
-
+    
+    if (isNaN(petNumber) || petNumber < 1 || petNumber > petsData.length) {
+        alert("Please enter a valid number between 1 and " + petsData.length);
+        return;
+    }
+    
     const petIndex = petNumber - 1;
     const selectedPet = petsData[petIndex];
-
-    const petInfo = '${selectedPet.petName} the ${selectedPet.breed} is ${selectedPet.age} years old. This pet weighs ${selectedPet.weightInKilos} kilos.';
-
+    
+    const petInfo = `${selectedPet.petName} the ${selectedPet.breed} is ${selectedPet.age} years old. This pet weighs ${selectedPet.weightInKilos} kilos.`;
+    
     const selectedPetInfo = document.querySelector('.selectedPetInfo');
     selectedPetInfo.textContent = petInfo;
 }
